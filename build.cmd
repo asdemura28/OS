77547ff0c1596e2124bb03171c3852dbe39b1925
asdemura28 <shurademura@mail.ru>
2025-01-15 21:46:32 +1000

    changed build.cmd

--- build.cmd
+++ build.cmd
@@ -1,6 +1,3 @@
-@echo off
-chcp 65001 >nul
-
 set "PROJECT_DIR=%cd%"
 set "BUILD_DIR=%PROJECT_DIR%\build"
 set "CMAKE_FILE=%PROJECT_DIR%\CMakeLists.txt"
@@ -10,32 +7,6 @@ if not exist "%BUILD_DIR%" (
 )
 
 cd "%BUILD_DIR%"
-
-
 cmake -G "MinGW Makefiles" "%CMAKE_FILE%"
-if %errorlevel% neq 0 (
-    echo Ошибка конфигурации CMake.
-	cd ..
-    exit /b %errorlevel%
-)
-
 mingw32-make
-if %errorlevel% neq 0 (
-    echo Ошибка сборки.
-	cd ..
-    exit /b %errorlevel%
-)
-
-echo Запуск программы через 5 секунд...
-timeout /t 5 >nul
-cls
-
-set "EXE_PATH=%BUILD_DIR%\myprog.exe"
-if exist "%EXE_PATH%" (
-    "%EXE_PATH%"
-) else (
-    echo Исполняемый файл не найден.
-)
-
 cd ..
-pause
